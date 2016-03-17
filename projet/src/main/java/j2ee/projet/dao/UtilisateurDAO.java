5f539a931794f83173dd7aae51985f50087f1da5
mtroysi <morgane.troysi@etud.univ-angers.fr>
2016-03-17 11:48:27 +0100

    DAO Utilisateur

--- projet/src/main/java/j2ee/projet/dao/UtilisateurDAO.java
+++ projet/src/main/java/j2ee/projet/dao/UtilisateurDAO.java
@@ -1,5 +1,33 @@
 package j2ee.projet.dao;
 
-public class UtilisateurDAO {
+import java.util.List;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+import j2ee.projet.domaine.Utilisateur;
+
+@Repository("utilisateurDao")
+@Transactional(propagation = Propagation.REQUIRED)
+public class UtilisateurDAO {
+	@PersistenceContext
+	private EntityManager entityManager;
+
+	public EntityManager getEntityManager() {
+		return entityManager;
+	}
+	
+	public void setEntityManager(EntityManager entityManager) {
+		this.entityManager = entityManager;
+	}
+		 
+	public void insert(Utilisateur user) {
+		entityManager.persist(user);
+	}
 }
