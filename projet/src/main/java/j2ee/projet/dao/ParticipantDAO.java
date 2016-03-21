200c135a211f1786bf97f9df7d751f311172bdd7
Florentin <flo.noel@hotmail.fr>
2016-03-21 23:19:15 +0100

    Architecture et test
    Arrangement architecture resources 
    Mise en place des tests des DAO

--- projet/src/main/java/j2ee/projet/dao/ParticipantDAO.java
+++ projet/src/main/java/j2ee/projet/dao/ParticipantDAO.java
@@ -8,18 +8,18 @@ import org.apache.log4j.Logger;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
-import j2ee.projet.domaine.Participant;
+import j2ee.projet.domaine.Participation;
 
 @Repository
 @Transactional
-public class ParticipantDAO extends BaseDAO<Participant> {
+public class ParticipantDAO extends BaseDAO<Participation> {
 final static Logger logger = Logger.getLogger(CommentaireDAO.class);
 	
-	public List<Participant> listingParticipations(int idprojet) 
+	public List<Participation> listingParticipations(int idprojet) 
 	{
 		if (getEntityManager()==null)
 			logger.info("entityManager null");
-		TypedQuery<Participant> query = getEntityManager().createNamedQuery("listingParticipations",Participant.class);
+		TypedQuery<Participation> query = getEntityManager().createNamedQuery("listingParticipations",Participation.class);
 		query.setParameter("idprojet", idprojet);
 		return query.getResultList();
 	}
