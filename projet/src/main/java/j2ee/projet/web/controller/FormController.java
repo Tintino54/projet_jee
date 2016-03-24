2ca7843c418ffc773cfa38bd6b63484859e60abd
ValentinGinisty <valentin.ginisty@gmail.com>
2016-03-24 11:50:24 +0100

    conflit

--- projet/src/main/java/j2ee/projet/web/controller/FormController.java
+++ projet/src/main/java/j2ee/projet/web/controller/FormController.java
@@ -4,13 +4,11 @@ import java.io.IOException;
 
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
-import j2ee.projet.domaine.Campagne;
 import j2ee.projet.domaine.Commentaire;
 import j2ee.projet.metier.CommentaireService;
 
